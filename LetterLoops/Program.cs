53f9718a444f0c79a55176fddd6bf331025a3b23
Leo Taylor <leotaylor723@gmail.com>
2018-08-27 17:26:48 -0500

    works

--- LetterLoops/Program.cs
+++ LetterLoops/Program.cs
@@ -6,7 +6,21 @@ namespace LetterLoops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Input Some Letters:");
+            var input = Console.ReadLine();
+            var result = "";
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                result += char.ToUpper(c);
+                result += new String(char.ToLower(c), i);
+                result += "-";
+            }
+            Console.WriteLine(result.TrimEnd('-'));
+
+            Console.WriteLine("Press Enter To Exit");
+            Console.ReadLine();
         }
     }
 }
